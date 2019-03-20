d2bd8542a9b2d91cf3eaab0f4095c5090e55868a
Bene "CrazyBene" Grau <Benedikt.grau@gmx.de>
2019-03-20 20:07:52 +0100

    Turret find nearest enemy to it

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField]
+    private float range = 10f;
+
+    [SerializeField]
+    private string enemyTag = "Enemy";
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null) 
+            return;
+
+        
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance) 
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range) 
+        {
+            target = nearestEnemy.transform;
+        } 
+        else
+        {
+            target = null;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        if(target != null) 
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+    }
+
+}
