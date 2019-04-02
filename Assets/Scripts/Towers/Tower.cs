f359503020ee61b3d091fc16b1e2c19edafe342b
Bene "CrazyBene" Grau <Benedikt.grau@gmx.de>
2019-04-02 23:12:06 +0200

    Barebones of an upgrade system
    The general structure for an uograde system for the towers, but needs some tweekings in name and file structuring

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -5,5 +5,46 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
   
+    // Make this some kind of central statc thing maybe
+    // Maybe a scribtableobject, or just a simple class
+    public TowerLevel towers;
+
+    private TowerLevel currentTowerLevel;
+
+    private GameObject currentTower;
+
+    void Awake()
+    {
+        SetTower(towers);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            var newTower = currentTowerLevel.upgradesTo[0];
+            SetTower(newTower);
+        }
+    }
+
+    private void SetTower(TowerLevel tower)
+    {
+        currentTowerLevel = tower;
+
+        if(currentTowerLevel != null)
+            Destroy(currentTower);
+
+        currentTower = Instantiate(currentTowerLevel.towerPrefab, transform.position, transform.rotation);
+        currentTower.transform.SetParent(transform);
+    }
+
+}
+
+[System.Serializable]
+public class TowerLevel 
+{
+
+    public GameObject towerPrefab;
+    public List<TowerLevel> upgradesTo;
 
 }
