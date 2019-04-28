aba78c5d6e841ade11168ba90ce735d615b98f3a
Bene "CrazyBene" Grau <Benedikt.grau@gmx.de>
2019-04-28 18:38:30 +0200

    Simple Messager system to show player messages (duh)

---
+++ Assets/Scripts/Messager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Messager : MonoBehaviour
+{
+
+    private static Messager instance;
+
+    [SerializeField]
+    private TextMeshProUGUI messageText;
+
+    [SerializeField]
+    private float showMessageTime = 3f;
+
+    private float lastMessageCountdown = 0f;
+
+    private bool isShowingMessage = false;
+
+    private Coroutine currentFadeIn;
+    private Coroutine currentFadeOut;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if(lastMessageCountdown <= 0 && isShowingMessage)
+        {
+            if(currentFadeIn != null)
+                StopCoroutine(currentFadeIn);
+            currentFadeOut = StartCoroutine(FadeOut(1f, messageText));
+            isShowingMessage = false;
+        }
+        else if(lastMessageCountdown > 0)
+        {
+            lastMessageCountdown -= Time.deltaTime;
+        }
+    }
+
+    private void ShowMessage(string message)
+    {
+        messageText.text = message;
+        lastMessageCountdown = showMessageTime;
+
+        if(!isShowingMessage)
+        {
+            isShowingMessage = true;
+            currentFadeIn = StartCoroutine(FadeIn(1f, messageText));
+            if(currentFadeOut != null)
+                StopCoroutine(currentFadeOut);
+        }
+    }
+    
+    public static void NewMessage(string message)
+    {
+        instance.ShowMessage(message);
+    }
+
+
+    // Helper function to fade text in and out
+    private IEnumerator FadeIn(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+ 
+    private IEnumerator FadeOut(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/OnGuiTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGuiTest : MonoBehaviour
+{
+    
+    void OnGUI () {
+		GUILayout.BeginArea(new Rect(4f, 4f, 150f, 500f));
+		if(GUILayout.Button("Send Message 1"))
+        {
+            Messager.NewMessage("Hallo");
+        }
+        if(GUILayout.Button("Send Message 2"))
+        {
+            Messager.NewMessage("World");
+        }
+		GUILayout.EndArea();
+	}
+
+}
